307e797d8a9dbe4218086e6e5bfb2af2a25c9819
fabian <farevalo.fabian@gmail.com>
2019-09-25 12:34:33 -0500

    mensaje

--- app/src/main/java/com/example/primer_app/MainActivity.java
+++ app/src/main/java/com/example/primer_app/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-    //prueba
+    //prueba final
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
